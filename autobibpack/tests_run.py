206e63d606ad77f814e2feb73b2a7b7a22993a9a
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-09-05 17:32:49 +0100

    Enable running of all available test suites.

---
+++ autobibpack/tests_run.py
@@ -0,0 +1,26 @@
+'''Run all available tests.'''
+import unittest
+
+# The modules with test suites
+import fetch_test_ora as s1
+import fetch_test_plos as s2
+import fetch_test_datafinder as s3
+
+# Define which test suites will be used.
+test_suites = [s1, s2, s3]
+
+# Run all the suites.
+def run(suites, verb=0):
+    '''Run the suites with no verbosity.'''
+    for suite in suites:
+        log('Running: %s'%suite.SUITE_NAME)
+        unittest.TextTestRunner(verbosity=verb).run(suite.suite())
+
+def log(message):
+    print
+    print message
+    print
+
+log('Running all available tests. ------------------')
+run(test_suites)
+log('Finished all available tests. -----------------')
