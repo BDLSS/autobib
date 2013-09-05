825576771edb9da3f5a9fcc5fa3312c87b430367
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2013-09-05 17:19:47 +0100

    Added fetching class and tests against ORA.

---
+++ autobibpack/fetch.py
@@ -0,0 +1,218 @@
+'''Search bibliographic sources and fetch content.'''
+import urllib # for encoding the url
+import urllib2 # for getting page
+import StringIO # for reading results
+import json # for converting json
+
+class Search(object):
+    '''A general class for searching for items.'''
+    def __init__(self, name='test_search'):
+        self.reset(name)
+        
+    def reset(self, name):
+        '''Resets this search item to defaults.'''
+        self.NAME = name
+        self.STATUS = 'needs an end point url'
+        self.QUERYCOLON = dict()  # to pass to q= with :
+        self.QUERY_AT = dict()    # to pass to endpoint with &
+        self.MAX_ROWS = 999 # how many records per request.
+        self.RAWDATA = dict()   # the last set of data returned.
+        self.DOCUMENTS = dict() # keys = document UUIDs in ORA, ID in others?
+        self.DOCS_FOUND = 0 # How many document found during the first search
+        self.NEXT_START = 0 # Where in the sequence are we
+        self.AND_JOINERS_TESTED = ['&', ' AND '] # What to use for AND queries
+        # It looks like ORA supports the first and PLoS uses the second.
+        self.AND_JOINER = self.AND_JOINERS_TESTED[0]
+        # The above line caused error in PLoS tests because [1] assumed. 
+        self.set_json() # assume we always want JSON
+        self.SEARCH_METHODS_AVAILABLE = {
+            'solr': ('author', 'title', 'id', 'simple', 'datetime', 'daterange')
+            }
+        # key = search interface, tuple = tested methods for searching
+
+    #  Set the attributes passed in the URL 
+    # ===============================================================    
+    def set_endpoint(self, url):
+        '''The main URL that runs the query.'''
+        self.END_POINT = url
+        self.STATUS = '' # now we can run the query.
+
+    # The rest of these are optional
+    def set_apikey(self, unique, url_key='api_key'):
+        '''Enable sources that require unique api keys to be passed in url.'''
+        self.set_at(url_key, unique)
+    
+    def set_at(self, key, value):
+        '''Add key and value to the dictionary used to generate URL.'''
+        self.QUERY_AT[key] = value
+        
+    def set_json(self):
+        '''Set JSON as the type of result to return.'''
+        self.set_at('wt', 'json')
+        
+    def set_indent(self, enable=False):
+        '''Enable indentation on results that get returned.'''
+        if enable:
+            self.set_at('indent', 'on')
+    
+    def set_rows(self, count):
+        '''Set the number of rows to return per query.'''
+        count = int(count)
+        if count > self.MAX_ROWS:
+            count = self.MAX_ROWS
+        self.set_at('rows', count)
+    
+    def set_start(self, count):
+        '''Set the start record to return for the query.'''
+        self.set_at('start', count)
+            
+    def set_field_getlist(self, fields):
+        '''Limit the returned results to specific fields.'''
+        self.set_at('fl',fields)
+         
+    def set_sort(self, key, descending=False):
+        '''Use key to sort returned results, with option to make decending.'''
+        if descending:
+            order = 'desc'
+        else:
+            order = 'asc'    
+        self.set_at('sort', '%s %s'%(key, order))
+        
+    #  Methods that alter the SOLR query (q) string.
+    # ===============================================================        
+    def query(self, field, value):
+        '''Add field and value to the query.'''
+        self.QUERYCOLON[field] = value
+        
+    def query_author(self, author):
+        self.query('author', author)
+    
+    def query_title(self, title, add_quotes=True):
+        '''Adds a quoted title search to the query with option to not quote.'''
+        if add_quotes:
+            title = '"%s"'%title
+        self.query('title', title)
+    
+    def query_simple(self, author, title):
+        self.query_author(author)
+        self.query_title(title)
+        
+    def query_id(self, itemid, idkeyname='id', prefix='uuid:'):
+        self.query(idkeyname, '"%s%s"'%(prefix, itemid))
+    
+    def format_date(self, year, month, day): # not in unittests
+        return '%s-%s-%s'%(year, month, day)
+        
+    def query_datetime(self, field, date=None, time='00:00:00.000Z'):
+        '''Query field for date with option set a specific time.'''
+        value = '"%sT%s"'%(date, time)
+        self.query(field, value)
+        
+    def query_daterange(self, field, start=None, end=None,
+                        usetime=True, digit3=True):
+        '''Query field for a date range from start to end with inclusive time.'''
+        if usetime:
+            # Tested 2 and 3 digits seconds with both ORA and PLoS. Both worked
+            # so went with three digits.
+            if digit3:
+                #Example from ORA    "timestamp":"2012-12-19T21:05:10.512Z"
+                #So, seconds is in three digits
+                value = '[%sT00:00:000Z TO %sT23:59:590Z]'%(start, end)
+            else:
+                # Example on: http://api.plos.org/solr/search-fields/
+                # has seconds to two digits.
+                value = '[%sT00:00:00Z TO %sT23:59:59Z]'%(start, end)
+        else:
+            #  See http://lucene.apache.org/core/2_9_4/queryparsersyntax.html#Range Searches
+            value = '[%s TO %s]'%(start, end)
+        self.query(field, value)
+
+    #  Generate the URL and fetch the data
+    # ===============================================================        
+    def make_colon(self):
+        '''Return the value for query (q) where items have colons.'''
+        answer = ''
+        addand = '' #use and to join query.
+        for item in self.QUERYCOLON:
+            answer += '%s%s:%s'%(addand, item, self.QUERYCOLON[item])
+            addand = self.AND_JOINER # Not all sources support the same joiner
+            # The colon will change to %3A when urlencoded.
+        return answer
+    
+    def make_and(self):
+        '''Return the encoded query string.'''
+        made = self.make_colon()
+        
+        # All requests have an 'q' attribute but could be left empty if
+        # user has not added any query items. So use wildcard.
+        if not made: #Tested with test126_make_and_noquery in ORA tests
+            made = '*:*'
+        self.QUERY_AT['q'] = made
+        # by adding 'q' above we can let urlencode deal with all options
+        return urllib.urlencode(self.QUERY_AT)
+        
+    def make_query(self):
+        '''Return the URL that should return data.'''
+        if not self.STATUS:
+            query = self.END_POINT
+            query += self.make_and()
+            return query
+        else:
+            return ''
+
+    def fetch_data(self):
+        '''Return the data from the course and cache it.'''
+        load = urllib2.urlopen(self.make_query())
+        self.RAWDATA = load.read()
+        load.close()
+        return self.RAWDATA
+
+    #  Process fetched data
+    # ===============================================================
+    def get_json_raw(self):
+        '''Return the data as JSON with response headers.'''
+        self.fetch_data()
+        data = StringIO.StringIO(self.RAWDATA)
+        return json.load(data)
+    
+    def get_documents(self):
+        '''Return a tuple containing items found and prepare next query.'''
+        data = self.get_json_raw()
+        documents = data['response']['docs']
+        
+        # Enable it so we can run the query again with different start point
+        if not self.DOCS_FOUND:
+            self.DOCS_FOUND = data['response']['numFound']
+        this_start = data['response']['start']
+        this_count = len(documents)
+        self.NEXT_START = int(this_start)+int(this_count)+1
+        
+        return documents
+
+    def get_all(self):
+        '''Get all the documents, doing muliple fetches when needed.'''
+        new_docs =  self.get_documents()
+        self.update_documents(new_docs)
+        while self.NEXT_START <= self.DOCS_FOUND:
+            self.set_start(self.NEXT_START)
+            new_docs = self.get_documents()
+            self.update_documents(new_docs)
+    
+    #  Post-processing of data
+    # ===============================================================        
+    def update_documents(self, new_documents):
+        '''Add new documents to internal list of documents found.'''
+        for doc in new_documents:
+            key = doc['id']
+            self.DOCUMENTS[key] = doc
+    
+    def pprint_json(self, text):
+        '''Print json text with small indents with keys sorted.'''
+        return json.dumps(text, sort_keys=True,
+                  indent=2, separators=(',', ': '))
+        
+if __name__ == '__main__':
+    import fetch_test_ora as get
+    import unittest
+    unittest.TextTestRunner(verbosity=1).run(get.suite())
+    
---
+++ autobibpack/fetch_test_ora.py
@@ -0,0 +1,325 @@
+'''Test the search interface against ORA using Solr.
+
+This tests the core features which should
+be the same for most sources.'''
+import unittest
+import fetch
+
+class TestOraBasic(unittest.TestCase):
+    '''Check the basic functionality of the Solr bibliographic search.'''          
+    def setUp(self):
+        self.NAME = 'ora_solr'
+        self.END = 'http://ora.ox.ac.uk:8080/solr/core_metadata/select/?'
+        self.FIELD = 'author'
+        # We are going to use the following thesis as our test item.
+        # http://ora.ox.ac.uk/objects/uuid:83530474-369e-417b-a8db-ac06ebf42c84
+        self.AUTHOR = 'cummings'
+        self.TITLE = 'neural control of convergence eye movements'
+        self.EID = '83530474-369e-417b-a8db-ac06ebf42c84'
+        self.SEARCH = fetch.Search(self.NAME)
+        self.DATEFIELD = 'timestamp'
+        self.EDATE = '2013-01-21'
+        self.ETIME = '11:14:22.227Z'
+        self.EDATETIME = '%sT%s'%(self.EDATE, self.ETIME)
+        self.JOINER = '&'
+        
+        # These expected value might change. Correct as of 18th July 2013
+        # The total expected is due to ORA defaulting return 10 rows.
+        self.TOTAL_EXPECTED = 10
+        self.ROWS_EXPECTED = 2
+        self.DOCS_EXPECTED = 334
+        self.TITLE_NOQUOTES_EXPECTED = 112100
+        self.TOTAL_DATERANGE = 250
+ 
+    #  Basic checks the query is constructed okay, before calling source
+    # ===============================================================
+    def test100_default(self):
+        self.assertEqual(self.NAME, self.SEARCH.NAME)
+        self.assertFalse(self.SEARCH.make_query())
+    
+    def do_endpoint(self):
+        self.SEARCH.set_endpoint(self.END)
+            
+    def test105_endpoint(self):
+        self.do_endpoint()
+        self.assertEqual(self.END, self.SEARCH.END_POINT)
+    
+    def check_query(self):
+        '''This will check the two ways of setting queries.'''
+        self.assertIn(self.FIELD, self.SEARCH.QUERYCOLON)
+        self.assertEqual(self.AUTHOR, self.SEARCH.QUERYCOLON[self.FIELD])
+        
+    def test110_query(self):
+        self.SEARCH.query(self.FIELD, self.AUTHOR)
+        self.check_query()  
+    
+    def do_author(self):
+        self.SEARCH.query_author(self.AUTHOR)
+        
+    def test115_query_author(self):
+        self.do_author()
+        self.check_query()
+        
+    def test120_make_colon(self):
+        self.do_author()
+        result = self.SEARCH.make_colon()
+        self.assertEqual(result, '%s:%s'%(self.FIELD, self.AUTHOR))
+        
+    def test125_make_and_default(self):
+        self.do_author()
+        result = self.SEARCH.make_and()
+        items = [self.FIELD, self.AUTHOR, 'wt', 'json']
+        for item in items:
+            self.assertIn(item, result)
+        
+    def test126_make_and_noquery(self):
+        self.do_endpoint()
+        result = self.SEARCH.make_and()
+        # the query should be *:* but this gets url encoded so test raw values
+        star = r'%2A'
+        colon = r'%3A'
+        self.assertTrue(result.startswith('q=%s%s%s'%(star, colon, star)))
+        
+    def show_query(self): 
+        self.SEARCH.set_indent()
+        made = self.SEARCH.make_query()
+        print
+        print made
+        print
+        
+    #  Call the source with query and do a simple check of results.
+    # ===============================================================
+    def do_prepare_query(self):
+        self.do_endpoint()
+        self.do_author()
+        
+    def test130_fetch_data(self):
+        self.do_prepare_query()
+        data = self.SEARCH.fetch_data()
+        #print len(data)
+        self.assertLess(20000, len(data)) # len=29543 as of July 18th 2013
+
+    def get_header_bit(self, header, bit):
+        head = 'responseHeader'
+        pa = 'params'
+        return header[head][pa][bit]
+            
+    def test135_get_json_response(self):
+        self.do_prepare_query()
+        answer = self.SEARCH.get_json_raw()
+        wt = self.get_header_bit(answer, 'wt')
+        q = self.get_header_bit(answer, 'q')
+        self.assertEqual(wt, 'json')
+        self.assertEqual(q, '%s:%s'%(self.FIELD, self.AUTHOR))
+    
+    def do_get_docs(self):
+        self.do_prepare_query()
+        self.assertEqual(self.SEARCH.DOCS_FOUND, 0)
+        return self.SEARCH.get_documents()
+    
+    def test140_get_documents(self):
+        docs = self.do_get_docs()
+        self.assertGreaterEqual(self.TOTAL_EXPECTED, len(docs))
+        self.assertGreaterEqual(self.SEARCH.DOCS_FOUND, self.DOCS_EXPECTED) 
+        
+    def test145_update_documents(self):
+        docs = self.do_get_docs()
+        self.SEARCH.update_documents(docs)
+        self.assertGreaterEqual(self.TOTAL_EXPECTED, len(docs))
+        
+    #  Test optional items in the query string
+    # ===============================================================
+    def do_row_limit(self):
+        self.SEARCH.set_rows(self.ROWS_EXPECTED) # change default query
+        
+    def test150_setrows(self):
+        self.do_prepare_query()
+        self.do_row_limit()
+        docs = self.SEARCH.get_documents()
+        self.assertGreaterEqual(self.ROWS_EXPECTED, len(docs))
+    
+    def test155_setstart(self):
+        self.do_prepare_query()
+        self.SEARCH.set_start(self.ROWS_EXPECTED)
+        answer = self.SEARCH.get_json_raw()
+        result = self.get_header_bit(answer, 'start')
+        self.assertEqual(str(self.ROWS_EXPECTED), result)
+
+    def test160_next_start(self):
+        self.do_prepare_query()
+        self.assertEqual(self.SEARCH.NEXT_START, 0)
+        self.do_get_docs()
+        self.assertEqual(self.SEARCH.NEXT_START, 11)
+    
+    def test165_next_start_url(self):
+        self.do_get_docs()
+        self.SEARCH.set_start(self.SEARCH.NEXT_START)
+        self.assertIn('start=11',self.SEARCH.make_query())
+        
+    #  Test title searches in the query string
+    # ===============================================================
+    def test170_title(self):
+        self.do_endpoint()
+        self.SEARCH.query_title(self.TITLE)
+        expected = '"%s"'%self.TITLE
+        self.assertEqual(expected, self.SEARCH.QUERYCOLON['title'])
+        bits = self.TITLE.split(' ')
+        made = self.SEARCH.make_query()
+        for bit in bits:
+            self.assertIn(bit, made) 
+    
+    def test175_title_getdata(self):
+        self.do_endpoint()
+        self.SEARCH.query_title(self.TITLE)
+        self.SEARCH.get_documents()
+        self.assertEqual(1, self.SEARCH.DOCS_FOUND)
+        
+    def test180_title_noquotes(self):
+        self.do_endpoint()
+        self.SEARCH.query_title(self.TITLE, False) # make it so quotes not used
+        self.assertEqual(self.TITLE, self.SEARCH.QUERYCOLON['title'])
+        bits = self.TITLE.split(' ')
+        made = self.SEARCH.make_query()
+        for bit in bits:
+            self.assertIn(bit, made)
+        
+    def test175_title_noquotes_data(self):
+        self.do_endpoint()
+        self.SEARCH.query_title(self.TITLE, False)
+        self.SEARCH.set_rows('2')
+        self.SEARCH.get_documents()
+        self.assertGreater(self.SEARCH.DOCS_FOUND, self.DOCS_EXPECTED)    
+    
+    #  Test other searches in query string
+    # ===============================================================
+    def test180_authortitle(self):
+        self.do_endpoint()
+        self.SEARCH.query_simple(self.AUTHOR, self.TITLE)
+        self.SEARCH.get_documents()
+        self.assertEqual(1, self.SEARCH.DOCS_FOUND)
+    
+    def do_uuid(self):
+        '''This will search ORA and test the returned UUID.'''
+        new_documents = self.SEARCH.get_documents()
+        self.SEARCH.update_documents(new_documents)
+        expected = 'uuid:%s'%self.EID
+        self.assertTrue(self.SEARCH.DOCUMENTS.has_key(expected))
+        
+    def test185_id_via_title_search(self):
+        self.do_endpoint()
+        self.SEARCH.query_title(self.TITLE)
+        self.do_uuid()
+    
+    def test190_id_via_query(self):
+        self.do_endpoint()
+        self.SEARCH.query_id(self.EID)        
+        self.do_uuid()
+        
+    #  Test date queries
+    # ===============================================================
+    def test195_date_query(self):
+        self.do_endpoint()
+        self.SEARCH.query_datetime(self.DATEFIELD, self.EDATE, self.ETIME)
+        self.do_uuid()
+        text = self.SEARCH.DOCUMENTS['uuid:%s'%self.EID]
+        expected = text[self.DATEFIELD]
+        self.assertEqual(expected, self.EDATETIME)
+            
+    def test200_default_andjoiner(self):
+        self.assertEqual(self.SEARCH.AND_JOINER, self.JOINER)
+        self.do_endpoint()
+        self.SEARCH.query_datetime(self.DATEFIELD, self.EDATE, self.ETIME)
+        query = self.SEARCH.make_query()
+        self.assertIn(str(self.JOINER).replace(' ', '+'), query)
+    
+    def test205_daterange(self):
+        self.do_endpoint()
+        self.SEARCH.query_daterange(self.DATEFIELD, self.EDATE, self.EDATE)
+        self.SEARCH.get_documents()
+        #self.show_query()
+        
+        # In theory, the number of items added on a certain day will not
+        # change. Also, since we never delete stuff from ORA, if this number
+        # does change it will only be to increase.
+        self.assertGreaterEqual(self.SEARCH.DOCS_FOUND, self.TOTAL_DATERANGE)
+        
+        # But, we can test the above theory. If this test fails you would
+        # need to check if bulk loading or something else is likely to
+        # have caused this. If the theory is wrong then you will have to
+        # disable this test or update TOTAL_DATERANGE with the latest number.
+        m = 'Num of items added to ORA on the test day has changed, why?' 
+        self.assertEqual(self.SEARCH.DOCS_FOUND, self.TOTAL_DATERANGE, m) 
+        
+    def test210_fieldlist(self):
+        self.do_endpoint()
+        self.do_author() 
+        self.SEARCH.set_field_getlist(('id,title'))
+        docs = self.SEARCH.get_documents()
+        self.assertEqual(self.SEARCH.DOCS_FOUND, self.DOCS_EXPECTED)
+        for item in range(len(docs)): # test each doc has both field.
+            data = docs[item]
+            self.assertEqual(len(data), 2)
+            self.assertTrue(data.has_key('id'))
+            self.assertTrue(data.has_key('title'))
+            self.assertTrue(str(data['id']).startswith('uuid')),
+            self.assertTrue(len(data['title']) > 5) #Check it is not empty
+    
+    #  Trigger printing of items, good for developing tests. 
+    # ===============================================================
+    def notest999_print_json(self):
+        self.do_prepare_query()
+        text = self.SEARCH.get_json_raw()
+        print self.SEARCH.pprint_json(text)
+        
+    def notest999_print_documents(self):
+        self.do_prepare_query()
+        text = self.SEARCH.get_documents()
+        print self.SEARCH.pprint_json(text)
+
+    
+class TestOraLonger(unittest.TestCase):
+    '''Check the basic functionality of the Solr bibliographic search.'''          
+    def setUp(self):
+        self.NAME = 'ora_solr'
+        self.END = 'http://ora.ox.ac.uk:8080/solr/core_metadata/select/?'
+        self.FIELD = 'author'
+        self.AUTHOR = 'cummings'
+        self.SEARCH = fetch.Search(self.NAME)
+    
+    def do_prepare_query(self):
+        self.SEARCH.set_endpoint(self.END)
+        self.SEARCH.query_author(self.AUTHOR)
+        
+    def notest160_all_rows(self):
+        self.do_prepare_query()
+        rwanted = self.SEARCH.MAX_ROWS
+        self.SEARCH.set_rows(rwanted)
+        text = self.SEARCH.get_documents()
+        expected = 334 # as of July 23rd 2013
+        self.assertGreaterEqual(len(text), expected)
+        
+    def notest170_get_all(self):
+        self.do_prepare_query()
+        self.SEARCH.get_all()
+        print len(self.SEARCH.DOCUMENTS)
+    
+    
+        
+# ===============================================================
+#  Enable use of these tests by external script.
+# ===============================================================
+SUITE_NAME = str(__name__)
+#TESTS_AVAILABLE = [TestOraBasic, TestOraLonger]
+TESTS_AVAILABLE = [TestOraBasic, ] # disable longer tests
+def suite(tests=TESTS_AVAILABLE):
+    '''Return a test suite of tests so this can run run by external script.'''
+    suite  = unittest.TestSuite()
+    for test in tests:
+        suite.addTests(unittest.TestLoader().loadTestsFromTestCase(test))
+    return suite 
+    
+if __name__ == '__main__':
+    # unittest.main(verbosity=2) # run all tests
+    #   OR
+    unittest.TextTestRunner(verbosity=2).run(suite()) # just those in suite
+        
