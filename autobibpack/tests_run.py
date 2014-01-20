d5e0dc062d9e2620dc31322b4ff02b64ef1a76b8
Martin Comina <martin.comina@bodleian.ox.ac.uk>
2014-01-20 14:24:05 +0000

    Disable testing of plos and datafinder.

--- autobibpack/tests_run.py
+++ autobibpack/tests_run.py
@@ -3,11 +3,12 @@ import unittest
 
 # The modules with test suites
 import fetch_test_ora as s1
-import fetch_test_plos as s2
-import fetch_test_datafinder as s3
+#import fetch_test_plos as s2
+#import fetch_test_datafinder as s3
 
 # Define which test suites will be used.
-test_suites = [s1, s2, s3]
+#test_suites = [s1, s2, s3]
+test_suites = [s1, ]
 
 # Run all the suites.
 def run(suites, verb=0):
